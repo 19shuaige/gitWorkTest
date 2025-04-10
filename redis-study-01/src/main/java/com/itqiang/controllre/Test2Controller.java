28f4e531db40f326c30347bf8eeb1695cbec9751
it-hdsqiang <14518413+it-hdsqiang@user.noreply.gitee.com>
2025-04-10 14:43:29 +0800

    我是B

---
+++ redis-study-01/src/main/java/com/itqiang/controllre/Test2Controller.java
@@ -0,0 +1,21 @@
+package com.itqiang.controllre;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ * 
+ * @author 伤口化作剑刃
+ * @date 2025/4/10 14:42
+ */
+@RequestMapping("/test2")
+@RestController
+public class Test2Controller {
+
+    @GetMapping
+    public String test2(){
+        return "test2";
+    }
+}
