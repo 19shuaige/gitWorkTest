67b62dd333c6c24813a6de455974ca615b1e390b
it-hdsqiang <14518413+it-hdsqiang@user.noreply.gitee.com>
2025-04-10 14:44:42 +0800

    我是B

--- redis-study-01/src/main/java/com/itqiang/controller/Test2Controller.java
+++ redis-study-01/src/main/java/com/itqiang/controller/Test2Controller.java
@@ -1,4 +1,4 @@
-package com.itqiang.controllre;
+package com.itqiang.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
